145c694c84511c83965d3e4901df9f255eb2bb2d
Dave Jal <foodauthority.dev@gmail.com>
2025-11-13 23:12:49 +0000

    c2

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 connectDB()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: true,
+  credentials: true,
+}))
 app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 
