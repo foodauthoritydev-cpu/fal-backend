a96c14667e74043dca13bc3d6cb7546afedd74a9
Dave Jal <foodauthority.dev@gmail.com>
2026-07-13 19:32:47 +0000

    732c1

--- server.js
+++ server.js
@@ -26,14 +26,16 @@ connectDB()
 const app = express()
 
 const corsOptions = {
-  origin: true,
+  origin: (origin, callback) => callback(null, origin || true),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  exposedHeaders: ['Set-Cookie'],
+  optionsSuccessStatus: 200
 }
 
 app.use(cors(corsOptions))
-app.options(/(.*)/, cors(corsOptions))
+app.options('*', cors(corsOptions))
 app.set('trust proxy', 1)
 app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ extended: true, limit: '100mb' }))
